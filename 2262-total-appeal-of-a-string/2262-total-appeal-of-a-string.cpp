3bf346dc2a107d84f23dfb6c9b2e8b59bb1528b2
brickcommander <73509938+brickcommander@users.noreply.github.com>
2022-08-11 23:48:25 +0530

    Time: 49 ms (56.66%), Space: 10.5 MB (77.57%) - LeetHub

---
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long appealSum(string s) {
+        long long ans = 0;
+        int n = s.size();
+        vector<int> prev(26, -1);
+        
+        for(int i=0; i<n; i++) {
+            int x = s[i] - 'a';
+            int l = i - prev[x] - 1, r = n - i - 1;
+            ans += l + r + 1 + l * r;
+            // cout << i << "-" << l << "-" << r << "-" << l+r+1+l*r << endl;
+            prev[x] = i;
+        }
+        
+        return ans;
+    }
+};
