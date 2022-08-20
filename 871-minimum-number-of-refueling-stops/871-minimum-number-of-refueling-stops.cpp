76e6c58c59a59aeb5a962e61ec763c123de00662
brickcommander <73509938+brickcommander@users.noreply.github.com>
2022-08-20 15:40:14 +0530

    Time: 50 ms (50.25%), Space: 16.2 MB (80.69%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
+    int NlogN_Approach(int target, int cur, vector<vector<int>> &s) {
+
+        int i = 0, res;
+        priority_queue<int>pq;
+        for (res = 0; cur < target; res++) {
+            while (i < s.size() && s[i][0] <= cur)
+                pq.push(s[i++][1]);
+            if (pq.empty()) return -1;
+            cur += pq.top(), pq.pop();
+        }
+        return res;
+    }
+    
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        return NlogN_Approach(target, startFuel, stations);
+        
         int n = stations.size();
         vector<long> dp(n+1, -1);
         dp[0] = startFuel;
